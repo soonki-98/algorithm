7090cf5c856f7b506bff3abaf8e646f1b3d6f76e
Soonki <alstnsrl98@gmail.com>
2022-01-19 14:25:44 +0900

    백준 node 풀이 세팅

---
+++ baekjoon/setting.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split("\n");
+
+function solution() {
+  // 풀이
+}
+
+console.log(solution());
