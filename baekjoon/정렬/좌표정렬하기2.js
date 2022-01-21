66b18d0bcfa31ec7609a9310311ab477746ebdfc
Soonki <alstnsrl98@gmail.com>
2022-01-21 10:45:35 +0900

    좌표 정렬하기2

---
+++ baekjoon/정렬/좌표정렬하기2.js
@@ -0,0 +1,14 @@
+const input = ["0 4", "1 2", "1 -1", "2 2", "3 3"];
+
+function solution() {
+  const location = input
+    .map((el) => el.split(" "))
+    .sort((a, b) => {
+      return Number(a[1]) === Number(b[1])
+        ? Number(a[0]) - Number(b[0])
+        : Number(a[1]) - Number(b[1]);
+    });
+  return location.join("\n").replace(/,/g, " ");
+}
+
+console.log(solution());
