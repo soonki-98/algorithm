48a589669af9af1a28e36eace3bad08622bec93c
Min <alstnsrl98@gmail.com>
2022-02-04 23:37:56 +0900

    setting

--- baekjoon/setting/여러줄.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().split("\n");
-
-function solution() {
-  // 풀이
-}
-
-console.log(solution());
