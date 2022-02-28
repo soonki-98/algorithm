3dd677c0a057316082e36e64dae4cde86c70e10c
Min <alstnsrl98@gmail.com>
2022-02-28 23:52:46 +0900

    통계학

--- baekjoon/정렬/통계학.js
+++ baekjoon/정렬/통계학.js
@@ -1,30 +1,36 @@
-// 시간 초과 메모리 초과
+// const input = 3;
+const arr = ["-1", "-2", "-3", "-1", "-2 "].join(" ").repeat(100000).split(" ");
+arr.pop();
+// const arr = ["4000"];
+// const arr = [1, 3, 8, -2, 2];
+// const arr = [0, 0, -1];
 
-const input = 5;
-const arr = ["-1", "-2", "-3", "-1", "-2"];
+console.time();
 
-const numbers = arr.map((el) => Number(el)).sort((a, b) => a - b);
 const obj = {};
-const result = [];
-const avg = Math.round(numbers.reduce((a, b) => a + b, 0) / input);
-const center = numbers[Math.floor(input / 2)];
-
-for (let num of numbers) {
-  if (obj[num]) obj[num] += 1;
-  else obj[num] = 1;
-}
-
-const sorted = Object.keys(obj)
-  .map((el) => Number(el))
+const numbers = arr
+  .map((el) => {
+    if (obj[el]) obj[el] += 1;
+    else obj[el] = 1;
+    return Number(el);
+  })
   .sort((a, b) => a - b);
 
-for (let i in sorted) {
-  if (Object.values(obj)[i] === Math.max(...Object.values(obj))) {
-    result.push(sorted[i]);
-  }
-}
+const modeTimes = Math.max(...Object.values(obj));
+const modeArr = [
+  ...new Set(
+    Object.keys(obj)
+      .sort((a, b) => a - b)
+      .filter((el) => obj[el] === modeTimes)
+  ),
+];
+const avg = Math.round(
+  numbers.reduce((acc, cur) => acc + cur, 0) / numbers.length
+);
 
-console.log(avg);
-console.log(center);
-console.log(result.length > 1 ? result[1] : result[0]);
-console.log(Math.max(...numbers) - Math.min(...numbers));
+console.log(avg === -0 ? 0 : avg);
+console.log(numbers[Math.floor(numbers.length / 2)]);
+console.log(modeArr[1] || modeArr[0]);
+console.log(numbers[numbers.length - 1] - numbers[0]);
+
+console.timeEnd();
